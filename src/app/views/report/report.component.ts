e328b279b86a0e609b8126fe3a2473a62984499d
Roberto Garcia <rg.rendon@hotmail.com>
2024-09-19 15:20:26 -0700

    design
    - added template elements
    - added view styles

--- src/app/views/report/report.component.ts
+++ src/app/views/report/report.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
+
+const ELEMENT_DATA: any[] = [
+  {request_date: '2024-19-09', type: 'MONTHLY', processing_day: 15, processing_date: '2024-10-15'},
+  {request_date: '2024-19-09', type: 'QUARTERLY', processing_day: 15, processing_date: '2024-10-15'},
+];
 
 @Component({
   selector: 'app-report',
@@ -22,6 +27,22 @@ import { MatTableModule } from '@angular/material/table';
 })
 export class ReportComponent {
   
-  constructor(private formBuilder: FormBuilder) {}
-  
+  public displayedColumns: string[] = ['request_date', 'type', 'processing_day', 'processing_date'];
+  public dataSource = ELEMENT_DATA;
+  public filters: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.filters = this.formBuilder.group({
+      type: ['', [Validators.required]],
+      day: ['', [Validators.required,Validators.min(1), Validators.max(31)]]
+    })
+  }
+
+  addReport() {
+    if (this.filters.valid) {
+      console.log(this.filters.value);
+    } else {
+      console.log('invalid');
+    }
+  }
 }

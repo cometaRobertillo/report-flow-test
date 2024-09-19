b2f17d614e2e70b6ab7d89cbeb50795cd1b3b6cb
Roberto Garcia <rg.rendon@hotmail.com>
2024-09-19 14:12:16 -0700

    create report view
    - implement report default component
    - create lazy loading for component view
    - add component to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./views/report/report.component').then(c => c.ReportComponent)
+    }
+];

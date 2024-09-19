829a569d25753b7284a7091ac69939a776300fc0
Roberto Garcia <rg.rendon@hotmail.com>
2024-09-19 14:20:24 -0700

    Add dependencies
    - Material component dependencies
    - Reactivity forms
    - Form Builders & validations

--- src/app/views/report/report.component.ts
+++ src/app/views/report/report.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatTableModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
 export class ReportComponent {
-
+  
+  constructor(private formBuilder: FormBuilder) {}
+  
 }
